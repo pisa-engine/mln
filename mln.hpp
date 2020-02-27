5cef6ce377f144775f5797ce6fa595e9b469a02c
Antonio Mallia <me@antoniomallia.it>
2020-02-26 19:20:25 -0500

    Added decoding

--- mln.hpp
+++ mln.hpp
@@ -9,7 +9,7 @@ template <size_t Q>
 class mln {
     using Table = std::array<std::array<uint8_t, Q>, Q>;
 
-    static Table generate_table(const uint32_t *in, const size_t n)
+    static Table generate_compression_table(const uint32_t *in, const size_t n)
     {
         std::array<std::array<uint32_t, Q>, Q> freq_table{};
 
@@ -34,11 +34,23 @@ class mln {
         }
         return t;
     }
+    static Table generate_decompression_table(Table &t)
+    {
+        Table dt{};
+        for (int i = 0; i < t.size(); ++i)
+        {
+            for (int j = 0; j < t[0].size(); ++j)
+            {
+                dt[i][t[i][j]-1] = j+1;
+            }
+        }
+        return dt;
+    }
 
    public:
-    static void encode(const uint32_t *in, const size_t n, uint32_t *out)
+    static Table encode(const uint32_t *in, const size_t n, uint32_t *out)
     {
-        auto t = generate_table(in, n);
+        auto t = generate_compression_table(in, n);
         out[0] = in[0];
         for (int i = 1; i < n; ++i) {
             auto prev = in[i - 1];
@@ -49,7 +61,21 @@ class mln {
                 out[i] = in[i];
             }
         }
+        return generate_decompression_table(t);
     }
 
-    static void decode(const uint32_t *in, uint32_t *out, size_t n) {}
+    static void decode(const uint32_t *in, uint32_t *out, size_t n, Table &t)
+    {
+        out[0] = in[0];
+        auto prev = out[0];
+        for (int i = 1; i < n; ++i) {
+            auto current = in[i];
+            if (prev <= Q and current <= Q) {
+                out[i] = t[prev - 1][current - 1];
+            } else {
+                out[i] = in[i];
+            }
+            prev = out[i];
+        }
+    }
 };
--- test_mln.cpp
+++ test_mln.cpp
@@ -6,13 +6,24 @@ int main(int argc, char const *argv[])
 {
 
     std::vector<uint32_t> input{1, 2, 3, 2, 1, 2, 1, 2, 8, 2, 9, 2};
-    std::vector<uint32_t> out(input.size());
+    std::cout << "Input values: " << std::endl;
+    for (auto &&v : input) {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
 
-    mln<8>::encode(input.data(), input.size(), out.data());
-    for (auto &&v : out) {
+    std::vector<uint32_t> encoded(input.size());
+    auto table = mln<8>::encode(input.data(), input.size(), encoded.data());
+    std::cout << "Encoded values: " << std::endl;
+    for (auto &&v : encoded) {
         std::cout << v << " ";
     }
     std::cout << std::endl;
     std::vector<uint32_t> decoded(input.size());
-    mln<8>::decode(out.data(), decoded.data(), out.size());
+    mln<8>::decode(encoded.data(), decoded.data(), encoded.size(), table);
+    std::cout << "Decoded values: " << std::endl;
+    for (auto &&v : decoded) {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
 }
